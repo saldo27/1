7a2b2077be9fc8bdbec48821c9c7c0a7b3538ed5
saldo27 <luisherrerapara@gmail.com>
2024-10-06 11:52:52 +0200

    models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+Traceback (most recent call last):
+  File "C:\Py\1\main.py", line 1, in <module>
+    from cli import run_cli
+  File "C:\Py\1\cli.py", line 2, in <module>
+    from shift_scheduler import schedule_shifts
+  File "C:\Py\1\shift_scheduler.py", line 1, in <module>
+    from models import Shift
+ModuleNotFoundError: No module named 'models'
