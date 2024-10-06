82dc3dd9a5455e74ba7ececabfaedcff69bea6d3
saldo27 <luisherrerapara@gmail.com>
2024-10-06 11:55:35 +0200

    models.py

--- models.py
+++ models.py
@@ -1,8 +1,19 @@
-Traceback (most recent call last):
-  File "C:\Py\1\main.py", line 1, in <module>
-    from cli import run_cli
-  File "C:\Py\1\cli.py", line 2, in <module>
-    from shift_scheduler import schedule_shifts
-  File "C:\Py\1\shift_scheduler.py", line 1, in <module>
-    from models import Shift
-ModuleNotFoundError: No module named 'models'
+class Worker:
+    def __init__(self, id, work_dates, percentage, group, incompatible_job, group_incompatibility, obligatory_coverage, day_off):
+        self.id = id
+        self.work_dates = work_dates
+        self.percentage = percentage if percentage != "" else 100.0
+        self.group = group
+        self.incompatible_job = incompatible_job
+        self.group_incompatibility = group_incompatibility
+        self.obligatory_coverage = obligatory_coverage
+        self.day_off = day_off
+
+class Shift:
+    def __init__(self, date, job, worker_id):
+        self.date = date
+        self.job = job
+        self.worker_id = worker_id
+
+    def __str__(self):
+        return f"Shift on {self.date} for job {self.job}: Worker {self.worker_id}"
