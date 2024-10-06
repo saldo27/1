261fb72b824075f30bc6086eea081e17097ec3b2
saldo27 <luisherrerapara@gmail.com>
2024-10-06 20:11:44 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 class Worker:
     def __init__(self, identification, working_dates=None, percentage_shifts=0, group=0, position_incompatibility=None,
-                 group_incompatibility=None, mandatory_guard_duty=None, unavailable_dates=None):
+                 group_incompatibility=None, obligatory_coverage=None, unavailable_dates=None):
         self.identification = identification
         self.working_dates = working_dates if working_dates else []
         self.percentage_shifts = percentage_shifts
@@ -26,7 +26,7 @@ class Worker:
 
     @staticmethod
     def from_user_input(identification, working_dates, percentage_shifts, group, position_incompatibility,
-                        group_incompatibility, mandatory_guard_duty, unavailable_dates):
+                        group_incompatibility, obligatory_coverage, unavailable_dates):
         working_dates = [datetime.strptime(date.strip(), "%d/%m/%Y-%d/%m/%Y") for date in working_dates.split(',') if date]
         position_incompatibility = position_incompatibility.split(',') if position_incompatibility else []
         group_incompatibility = group_incompatibility.split(',') if group_incompatibility else []
