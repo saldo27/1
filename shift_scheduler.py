fa8676f65a71e4e1213826fb690dd578d920163e
saldo27 <luisherrerapara@gmail.com>
2024-10-06 16:54:57 +0200

    Update shift_scheduler.py

--- shift_scheduler.py
+++ shift_scheduler.py
@@ -50,11 +50,11 @@ def can_work_on_date(worker, date, last_shift_date, weekend_tracker, holidays_se
         week_number = date.isocalendar()[1]
         if weekly_tracker[worker.identification][week_number] >= 2:
             logging.debug(f"Worker {worker.identification} cannot work on {date} due to weekly quota limit.")
-                return False
+            return False
 
         if job in job_count[worker.identification] and job_count[worker.identification][job] > 0 and (date - last_shift_date[worker.identification]).days == 1:
             logging.debug(f"Worker {worker.identification} cannot work on {date} due to job repetition limit.")
-                return False
+            return False
 
     return True
 
