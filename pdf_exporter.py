6b8e5a306800bfdc3bcece575368a0f80c9ba592
saldo27 <luisherrerapara@gmail.com>
2024-10-09 18:16:48 +0200

    Update pdf_exporter.py

--- pdf_exporter.py
+++ pdf_exporter.py
@@ -25,11 +25,11 @@ class PDFCalendar(FPDF):
         self.set_font('Arial', '', 10)
 
         for week in month_days:
+            x_start = self.get_x()
             y_start = self.get_y()
             max_y = y_start
 
             for day in week:
-                x_start = self.get_x()
                 if day == 0:
                     self.cell(25, 40, '', 1, 0, 'C')
                 else:
@@ -41,7 +41,6 @@ class PDFCalendar(FPDF):
                     self.multi_cell(25, 10, "\n".join(lines), 1, 'C')
                     max_y = max(max_y, self.get_y())
                     self.set_xy(x_start + 25, y_start)
-                self.set_xy(x_start + 25, y_start)
 
             self.set_y(max_y)
             self.ln()
