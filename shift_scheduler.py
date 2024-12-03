ca6282b98d1a5bf941f703864ed19d9e3d9022eb
saldo27 <luisherrerapara@gmail.com>
2024-12-03 20:32:39 +0100

    Update shift_scheduler.py

--- shift_scheduler.py
+++ shift_scheduler.py
@@ -211,7 +211,7 @@ def schedule_shifts(work_periods, holidays, jobs, workers, min_distance, max_shi
                     assign_worker_to_shift(worker, date, job, schedule, last_shift_dates, weekend_tracker, weekly_tracker, job_count, holidays_set, min_distance, max_shifts_per_week)
                     last_assigned_job[worker.identification] = job
                     last_assigned_day[worker.identification] = date.weekday()
-                    day_rotation_tracker[w.identification][date.weekday()] = True
+                    day_rotation_tracker[worker.identification][date.weekday()] = True
                     assigned = True
 
                     iteration_count += 1
